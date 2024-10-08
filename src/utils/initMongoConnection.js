d18dc19462d3d607bce06f4d202d0dca69a3807c
Andrej Sokolov <99323383+CyberPanda263@users.noreply.github.com>
2024-10-08 10:53:36 +0300

    1.01

--- src/utils/initMongoConnection.js
+++ src/utils/initMongoConnection.js
@@ -11,8 +11,8 @@ export const initMongoConnection = async () => {
         const db = env(MONGO_DB_VARS.MONGO_DB);
 
         await mongoose.connect(`mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`);
-
-        console.log('MongoDB connection successfuly');
+        console.log('Mongo connection successfully established!');
+        
     } catch (error) {
         console.log('Error while connection to MongoDB', error);
         throw error;
